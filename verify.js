1315b2384270bf336bc99ee9f3adfeaa6a9c3c7d
AWA-DEV <44774769+DeveloperSeason@users.noreply.github.com>
2025-07-01 14:48:06 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,16 +1,8 @@
-import kotlin.jvm.JvmInline
-
-@OptIn(ExperimentalEncodingApi::class)
-private fun String.b64UrlDecodeToString(): String {
-    val normalized = padEnd(length + (4 - length % 4) % 4, '=')
-        .replace('-', '+').replace('_', '/')
-    val bytes = kotlin.io.encoding.Base64.Default.decode(normalized)
-    return bytes.decodeToString()
-}
-
-private val kbPayload: JsonObject? = keyBindingJwt?.let { jwt ->
+import id.walt.sdjwt.utils.Base64Utils.base64UrlDecode
+ private val kbPayload: JsonObject? = keyBindingJwt?.let { jwt ->
         runCatching {
-            val payloadB64 = jwt.split(".")[1]
-            payloadB64.b64UrlDecodeToString().let { Json.parseToJsonElement(it).jsonObject }
+            val payloadB64Url = jwt.split(".")[1]
+            val json = payloadB64Url.base64UrlDecode().decodeToString()
+            Json.parseToJsonElement(json).jsonObject
         }.getOrNull()
     }
