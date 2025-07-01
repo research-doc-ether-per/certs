298fb93e70d41f2bfb6c7a144c5a80c457cee8b3
AWA-DEV <44774769+DeveloperSeason@users.noreply.github.com>
2025-07-01 14:01:26 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,1 +1,19 @@
 
+init {
+      println(
+          """
+          ===== SDJwtVC created =====
+          holderDid     = $holderDid
+          holderKeyJWK  = $holderKeyJWK
+          issuer        = $issuer
+          nbf / exp     = $notBefore / $expiration
+          vct           = $vct
+          status        = $status
+          ---------------------------
+          undisclosed   = $undisclosedPayload
+          digestedHash  = ${sdPayload.digestedDisclosures}
+          disclosures   = $disclosures
+          ===========================
+          """.trimIndent()
+      )
+  }
