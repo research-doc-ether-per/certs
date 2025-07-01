2855f685db2ba2a2072be36b58d91a0a6fc4c20b
AWA-DEV <44774769+DeveloperSeason@users.noreply.github.com>
2025-07-01 16:20:48 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,9 +1,12 @@
-    val kbPayload: JsonObject? by lazy {
-        keyBindingJwt?.let { jwt ->
-            runCatching {
-                val payloadB64 = jwt.split(".")[1]                 // 取第二段
-                val jsonText   = payloadB64.base64UrlDecode().decodeToString()
-                Json.parseToJsonElement(jsonText).jsonObject
-            }.getOrNull()
-        }
+val kbPayload: JsonObject? = sdJwtVC.keyBindingJwt
+    ?.let { kbJwt ->
+        runCatching {
+            val payloadB64Url = kbJwt
+                .substringAfter('.')
+                .substringBeforeLast('.')
+            val jsonText = payloadB64Url
+                .base64UrlDecode()
+                .decodeToString()
+            Json.parseToJsonElement(jsonText).jsonObject
+        }.getOrNull()
     }
