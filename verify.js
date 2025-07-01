d383ab2bb879500422e29057e1538f3b07bc7244
AWA-DEV <44774769+DeveloperSeason@users.noreply.github.com>
2025-07-01 14:23:21 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,1 +1,34 @@
+import com.nimbusds.jwt.SignedJWT    
+ private val kbJwtString: String? get() = keyBindingJwt        
+    private val kbJwtPayloadJson: String? by lazy {
+        kbJwtString?.let {
+            runCatching {                                        
+                val payloadObj = SignedJWT.parse(it).payload.toJSONObject()
+                payloadObj.toJSONString()                        
+            }.getOrElse { ex -> "!! KB-JWT parse error: ${ex.message}" }
+        }
+    }
 
+    init {
+        println(
+            """
+            ===== SDJwtVC created =====
+            holderDid       = $holderDid
+            holderKeyJWK    = $holderKeyJWK
+            issuer          = $issuer
+            nbf / exp       = $notBefore / $expiration
+            vct             = $vct
+            status          = $status
+            -- undisclosedPayload --
+            $undisclosedPayload
+            -- digestedHashes --
+            ${sdPayload.digestedDisclosures}
+            -- disclosures --
+            $disclosures
+            -- KB-JWT payload --
+            $kbJwtPayloadJson
+            ==========================
+            """.trimIndent()
+        )
+    }
+}
