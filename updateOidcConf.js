b96eac186c95f13206fdf93fcfe83b711b97cdba
AWA-DEV <44774769+DeveloperSeason@users.noreply.github.com>
2025-07-09 18:07:15 +0900

    Update updateOidcConf.js

--- updateOidcConf.js
+++ updateOidcConf.js
@@ -1,1 +1,25 @@
-
+const config = {
+  issuerBaseURL: 'https://…/realms/wallet-personal',
+  baseURL:       'https://api.example.com',
+  clientID:      '…',
+  secret:        '…',
+  authRequired:  false,
+  routes: {
+    login:    false,
+    logout:   false,
+    callback: false,
+  },
+  auth0Logout: true,
+  authorizationParams: {
+    response_mode: 'query',
+    response_type: ['token'],
+  },
+  session: {
+    store: false,
+  },
+  clientAuthMethod: 'client_secret_post',
+  tokenIntrospection: {
+    endpoint:    '/protocol/openid-connect/token/introspect',
+    grant_type:  'client_credentials',
+  },
+};
