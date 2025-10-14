dcca2f3f34fc967e2632f2a7f48f77f57c8bb811
AWA-DEV <44774769+DeveloperSeason@users.noreply.github.com>
2025-10-14 14:46:09 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,1 +1,19 @@
 
+import id.walt.policies.policies.status.Values
+
+private val allowedSubjectTypes = setOf(
+    Values.BITSTRING_STATUS_LIST,
+    Values.STATUS_LIST_2021,
+    Values.REVOCATION_LIST_2020
+)
+
+
+override fun customValidations(statusList: W3CStatusContent, attribute: W3CStatusPolicyAttribute) {
+    val t = statusList.type // credentialSubject.type
+    require(t in allowedSubjectTypes) {
+        "Unsupported status list type: $t (expected one of $allowedSubjectTypes)"
+    }
+    require(statusList.purpose == attribute.purpose) {
+        "statusPurpose mismatch: ${statusList.purpose} != ${attribute.purpose}"
+    }
+}
