96abb1d1265d3ae36154b191eaaaa0f3f08ee88b
AWA-DEV <44774769+DeveloperSeason@users.noreply.github.com>
2025-10-15 22:20:57 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,1 +1,6 @@
+val statusEntries = when (data) {
+    is JsonArray -> data.map { w3cListEntryContentParser.parse(it) } // 支持数组
+    is JsonObject -> listOf(w3cListEntryContentParser.parse(data))   // 单一对象
+    else -> emptyList()
+}
 
