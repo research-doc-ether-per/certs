05b4203d991f29d756374f215a52f82bba883368
AWA-DEV <44774769+DeveloperSeason@users.noreply.github.com>
2025-10-31 15:06:02 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,1 +1,14 @@
+function buildActualCredentialUrl(vcRegistryBaseUrl, issuerDid, bslVcUrl) {
+  const u = new URL(vcRegistryBaseUrl);
+  const segs = ['issuers', issuerDid, 'bsl', 'vcUrls', bslVcUrl, 'credential']
+    .map(s => encodeURIComponent(String(s)));
 
+  // 去掉现有 pathname 末尾的斜杠，安全拼接
+  u.pathname = [u.pathname.replace(/\/+$/, ''), ...segs].join('/');
+  return u.toString();
+}
+
+
+
+const actualCredentialUrl =
+  `${vcRegistryBaseUrl.replace(/\/+$/, '')}/admin/issuers/${encodeURIComponent(issuerDid)}/bsl/vcUrls/${encodeURIComponent(bslVcUrl)}/credential`;
