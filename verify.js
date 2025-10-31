2e8927026c997f13a535d42530f97303b54de0d5
AWA-DEV <44774769+DeveloperSeason@users.noreply.github.com>
2025-10-31 22:08:43 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,1 +1,17 @@
+import kotlinx.serialization.json.JsonArray
+import kotlinx.serialization.json.JsonElement
+
+private suspend fun processStatusEntry(data: JsonElement, args: StatusPolicyArgument) = when (args) {
+    is IETFStatusPolicyAttribute -> processIETF(data, args)
+    is W3CStatusPolicyAttribute ->
+        if (data is JsonArray) {
+            processListW3C(
+                data,
+                W3CStatusPolicyListArguments(list = listOf(args))
+            )
+        } else {
+            processW3C(data, args)
+        }
+    is W3CStatusPolicyListArguments -> processListW3C(data, args)
+}
 
