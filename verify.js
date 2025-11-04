4e27d019f5896caf9d1809cc07ab8f83584d32fe
AWA-DEV <44774769+DeveloperSeason@users.noreply.github.com>
2025-11-04 16:25:09 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,7 +1,1 @@
-  array.forEachIndexed  val statusEntries: List<W3CEntry> = try {
-        w3cListEntryContentParser.parse(array)
-    } catch (e: Exception) {
-        throw IllegalArgumentException(
-            "Failed to parse credentialStatus array into List<W3CEntry>: ${e.message}", e
-        )
-    }
+
