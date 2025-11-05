bc0d3dbd85b8ac44f56399b7c5fe918571b7bb1f
AWA-DEV <44774769+DeveloperSeason@users.noreply.github.com>
2025-11-05 16:49:02 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,1 +1,16 @@
+function buildExactLikeYouWant(base, issuerDid, bslVcUrl) {
+
+  const trimRight = (s) => String(s).replace(/\/+$/g, "");
+
+  const trimLeft = (s) => String(s).replace(/^\/+/g, "");
+
+  const escapePercentOnly = (s) => String(s).replace(/%/g, "%25");
+
+  const baseClean = trimRight(base);
+  const issuerEscaped = escapePercentOnly(issuerDid);
+  const bslClean = trimLeft(bslVcUrl);
+
+
+  return `${baseClean}/issuers/${issuerEscaped}/bsl/vcUrls/${bslClean}//credential`;
+}
 
