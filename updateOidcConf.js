d29b0d2286a0cf15676fcb2d49037237e2add7cf
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-01-05 14:10:05 +0900

    Update updateOidcConf.js

--- updateOidcConf.js
+++ updateOidcConf.js
@@ -1,1 +1,18 @@
 
+import kotlinx.datetime.Clock
+import kotlinx.datetime.toJavaInstant
+import kotlinx.datetime.toKotlinInstant
+import java.time.ZoneOffset
+import java.time.ZonedDateTime
+import kotlin.time.Duration.Companion.hours
+
+
+  
+  val now = Clock.System.now()
+  val notBefore = now + 24.hours
+  val notAfter = ZonedDateTime.ofInstant(notBefore.toJavaInstant(), ZoneOffset.UTC)
+    .plusYears(1)
+    .toInstant()
+    .toKotlinInstant()
+
+ 
