389a761b01a529c562300c9de72da2daf4d5c5f6
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-01-26 14:57:34 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,1 +1,26 @@
+import fs from "fs/promises";
+import path from "path";
+
+const dirPath = "./data"; 
+
+const processFile = async (filePath, fileName) => {
+  const content = await fs.readFile(filePath, "utf-8");
+  console.log(`fileName: ${fileName}`);
+  // console.log(content);
+};
+
+const run = async () => {
+  const files = await fs.readdir(dirPath);
+
+  for (const fileName of files) {
+    const filePath = path.join(dirPath, fileName);
+
+    const stat = await fs.stat(filePath);
+    if (!stat.isFile()) continue; 
+
+    await processFile(filePath, fileName);
+  }
+};
+
+run().catch(console.error);
 
