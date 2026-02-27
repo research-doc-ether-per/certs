302792adfa8c83d89ed00a6ced37ef4e0161a3dc
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-02-27 21:48:49 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,19 +1,1 @@
-// https://datatracker.ietf.org/doc/html/draft-ietf-oauth-sd-jwt-vc-12
-const mergeByKey = (arr1, arr2) => {
-  const map = new Map()
 
-  ;[...arr1, ...arr2].forEach(item => {
-    if (map.has(item.key)) {
-      map.set(item.key, {
-        ...map.get(item.key),
-        ...item
-      })
-    } else {
-      map.set(item.key, item)
-    }
-  })
-
-  return Array.from(map.values())
-}
-
-const result = mergeByKey(arr1, arr2)
