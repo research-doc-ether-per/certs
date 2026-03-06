9188893f1c3634759600abd306a5362349a974ca
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-03-06 19:51:55 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,15 +1,1 @@
-return {
-      subject: cert.subject,
-      issuer: cert.issuer,
-      validFrom: cert.validFrom,
-      validTo: cert.validTo,
-      validFromDate: cert.validFromDate,
-      validToDate: cert.validToDate,
-      serialNumber: cert.serialNumber,
-      fingerprint: cert.fingerprint,
-      fingerprint256: cert.fingerprint256,
-      fingerprint512: cert.fingerprint512,
-      keyUsage: cert.keyUsage,
-      subjectAltName: cert.subjectAltName,
-      infoAccess: cert.infoAccess
-    }
+
