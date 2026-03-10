f3054ad4fe23347ce88cc741035806a982dfc6d0
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-03-10 11:08:59 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,1 +1,26 @@
 
+{
+  "iaca": {
+    "certificateData": {
+      "country": "JP",
+      "commonName": "日本モバイル運転免許証 IACA",
+      "issuerAlternativeNameConf": {
+        "uri": "https://ca.mdl.go.jp"
+      },
+      "notBefore": "2026-01-01T00:00:00Z",
+      "notAfter": "2041-01-01T00:00:00Z"
+    }
+  },
+  "documentSigner": {
+    "certificateData": {
+      "country": "JP",
+      "stateOrProvinceName": "東京都",
+      "localityName": "千代田区",
+      "organizationName": "デジタル庁",
+      "commonName": "モバイル運転免許証 テスト発行局",
+      "crlDistributionPointUri": "https://crl.mdl.go.jp/ds.crl",
+      "notBefore": "2026-01-01T00:00:10Z",
+      "notAfter": "2026-06-01T00:00:00Z"
+    }
+  }
+}
