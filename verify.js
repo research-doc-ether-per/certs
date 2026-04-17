deea1fc4024d0bcac3a4b9d6c4de752256c94fb4
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-04-17 20:26:35 +0900

    Remove unused finalVcJson construction

--- verify.js
+++ verify.js
@@ -1,10 +1,1 @@
 
-val finalVcJson = buildJsonObject {
-    injectedStatusVc.forEach { (key, value) ->
-        put(key, value)
-    }
-    if (display != null && display.isNotEmpty()) {
-        put("display", display)
-    }
-}
-log.info { "finalVcJson = $finalVcJson" }
