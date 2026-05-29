e422a317c7d1f958ffa54a76a3cba66e94b27be2
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-05-29 10:58:53 +0900

    Update updateOidcConf.js

--- updateOidcConf.js
+++ updateOidcConf.js
@@ -1,15 +1,1 @@
 
-module.exports = {
-  async headers() {
-    return [
-      {
-        source: "/.well-known/:path*", 
-        headers: [
-          { key: "Access-Control-Allow-Origin", value: "*" }, 
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS" },
-          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
-        ]
-      }
-    ]
-  }
-};
