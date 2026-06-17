a358caa9125ae1ccc35de9632b78104b867bb1ae
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-06-17 17:30:35 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,9 +1,1 @@
-指定されたユーザについて、Issuer から取得可能な Credential Offer URL、または発行可能な証明書情報の一覧を取得する。
 
-
-指定されたユーザについて、Credential Offer URL が指定されている場合、指定された Credential Offer URL に基づいて Issuer から発行可能な証明書情報を取得する。
-Credential Offer URL が指定されていない場合、発行可能証明書IDに基づいて Issuer に Credential Offer URL を要求し、取得した Credential Offer URL に基づいて Issuer から発行可能な証明書情報を取得する。
-
-
-
-指定されたユーザについて、Issuer から取得した発行可能な証明書情報に基づいて Issuer に証明書の発行を要求し、発行された証明書を指定されたユーザが利用可能なウォレットに保存する。
