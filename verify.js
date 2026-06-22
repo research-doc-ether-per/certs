513b56e8b880c7f225f41f028ec3db2fdaed764e
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-06-22 17:42:14 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,17 +1,24 @@
+sudo apt update
 
-sudo apt install -y \
-  curl \
-  wget \
-  git \
-  unzip \
-  zip \
-  build-essential \
-  net-tools \
-  ca-certificates \
-  gnupg \
-  lsb-release \
-  openssh-server
+sudo apt install -y wget gpg apt-transport-https
+
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+
+echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | \
+sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+
+rm -f packages.microsoft.gpg
+
+sudo apt update
+
+sudo apt install -y code
 
 
-sudo systemctl enable ssh
-sudo systemctl start ssh
+cd ~/Downloads
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+sudo apt install -y ./google-chrome-stable_current_amd64.deb
+
