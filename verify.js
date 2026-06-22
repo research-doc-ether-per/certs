80db0fe590cb95bf2d5bbed9c67a5694216ef1c6
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-06-22 16:57:29 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,1 +1,17 @@
 
+sudo apt install -y \
+  curl \
+  wget \
+  git \
+  unzip \
+  zip \
+  build-essential \
+  net-tools \
+  ca-certificates \
+  gnupg \
+  lsb-release \
+  openssh-server
+
+
+sudo systemctl enable ssh
+sudo systemctl start ssh
