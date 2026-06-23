bad986c1683bf302b8630cfe07b0929012d58790
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-06-23 15:57:22 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,6 +1,1 @@
-モバイル運転免許証（mDL）の発行に必要な Issuer の登録処理をステップ順に行う
-IACA 証明書発行: mDL 用のルート証明書を発行
 
-Document Signer（DS）証明書発行: 発行された IACA の鍵情報を用いて、データ署名用の DS 証明書を発行
-
-Issuer 作成: 鍵タイプに secp256r1、DID メソッドに web 形式を指定した Issuer を作成
