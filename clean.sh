c93fb20ced8b888e8f1c04c66d7a058dab847606
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-06-23 13:24:17 +0900

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -5,3 +5,11 @@ sudo fallocate -l 12G /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
+
+sudo apt update
+sudo apt install -y virtualbox-guest-x11 virtualbox-guest-utils virtualbox-guest-dkms
+sudo reboot
+
+lsmod | grep vbox
+
+
