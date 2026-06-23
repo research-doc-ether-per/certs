eaf112cbe4bc80748bcb80e9833c5fbe0478a13f
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-06-23 13:27:28 +0900

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -7,7 +7,13 @@ sudo mkswap /swapfile
 sudo swapon /swapfile
 
 sudo apt update
-sudo apt install -y virtualbox-guest-x11 virtualbox-guest-utils virtualbox-guest-dkms
+sudo apt install -y virtualbox-guest-x11 virtualbox-guest-utils
+sudo reboot
+
+sudo apt install -y dkms build-essential linux-headers-$(uname -r)
+
+cd /media/$USER/VBox_GAs_*
+sudo ./VBoxLinuxAdditions.run
 sudo reboot
 
 lsmod | grep vbox
