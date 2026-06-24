a9f168af478b096dac723c301d3c8f7a097b40a4
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-06-24 19:43:15 +0900

    Remove JWT verification checks from verify.js

--- verify.js
+++ verify.js
@@ -1,5 +1,1 @@
-"jwt_vc_json": [
-  "jwt_vc_json/audience-check",     // オーディエンス（送信先が自分であるか）を検証
-  "jwt_vc_json/nonce-check",        // リプレイアタック防止用のNonceが一致するかを検証
-  "jwt_vc_json/envelope_signature"  // ウォレットが作成したエンベロープ（器）の署名を検証
-]
+
