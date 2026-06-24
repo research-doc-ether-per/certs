f8bcee89b9d38240c198042eede81495ee75b9b8
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-06-24 19:22:19 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,1 +1,36 @@
 
+{
+  "flow_type": "cross_device",
+  "core_flow": {
+    "dcql_query": {
+      "credentials": [
+        {
+          "id": "1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed",
+          "format": "dc+sd-jwt",
+          "meta": {
+            "vct_values": [
+              "http://waltid.enterprise.localhost:3000/v1/waltid.issuer/issuer-service-api/openid4vc/draft13/identity_credential"
+            ]
+          }
+        }
+      ]
+    },
+    "policies": {
+      "vp_policies": {
+        "jwt_vc_json": [],
+        "dc+sd-jwt": [
+          "dc+sd-jwt/audience-check",
+          "dc+sd-jwt/kb-jwt_signature",
+          "dc+sd-jwt/nonce-check",
+          "dc+sd-jwt/sd_hash-check"
+        ],
+        "mso_mdoc": []
+      },
+      "vc_policies": [
+        { "policy": "signature" },
+        { "policy": "expiration" },
+        { "policy": "not-before" }
+      ]
+    }
+  }
+}
