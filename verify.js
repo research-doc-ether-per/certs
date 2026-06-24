97dff0acc762e005d0902d872f9f8a79f4b01866
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-06-24 17:25:35 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,1 +1,4 @@
 
+// 1.POST /verification-session/create createVerificationSession
+// 2.POST /verification-session/{verification-session}/response verifyVerificationSessionResponse
+// 3.GET /verification-session/{verification-session}/info getVerificationSessionInfo
