0918c620b852c8f37ec38f959d5164518873204a
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-06-25 14:18:20 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,7 +1,1 @@
-did-web-server:
-    image: nginx:alpine
-    container_name: did-web-server
-    ports:
-      - "5101:80"
-    volumes:
-      - ${HOME}/workspace/cloudcredentialservice/samples/v0.21.0/output/issuerDids:/usr/share/nginx/html/dids:ro
+
