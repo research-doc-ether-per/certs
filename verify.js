12ba8cd5ed33aa8cd50c5594a11528db040ea6c5
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-06-26 13:43:44 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,4 +1,4 @@
+v0.21.0 のホルダーウォレットでは、ホルダーDIDが異なる証明書の組み合わせ提示はサポートされていません。
 
-VICAL は mdoc の信頼リストを取得・検証するための機能で、SSE や Webhook は検証結果などをリアルタイムに通知するための仕組み
-
-
+原因として、ウォレット側（SSIKit2WalletService）の処理で、選択されたすべての証明書のホルダー公開鍵（Holder Binding）が完全に一致していることを必須条件としているためです。
+https://github.com/walt-id/waltid-identity/blob/d53d92cf3434925c8a2af103bf2590ddb0f7bf2f/waltid-services/waltid-wallet-api/src/main/kotlin/id/walt/webwallet/service/SSIKit2WalletService.kt#L1216
