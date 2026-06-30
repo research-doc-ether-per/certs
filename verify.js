c2416993f6afd3dd316f530e443973481b5cfdaa
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-06-30 16:30:33 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,8 +1,20 @@
-**id**
-提示可能証明書ID
-Presentation Request URL の発行対象となる提示可能証明書IDを指定する。
 
+**groupId**
+グループID
+VP を保存する Verifier が所属するグループIDを指定する。
 
-**userName**
-提示可能証明書対象者名
-Presentation Request URL の発行対象となるユーザ名を指定する。
+**state**
+state値
+Presentation Request URL から取得した state 値を指定する。
+
+**vp_token**
+VP（Verifiable Presentation）トークン
+提示された VP を表す JWT トークンを指定する。
+
+**user_id**
+VP提示ユーザID
+VP を提示したユーザIDを指定する。
+
+**verify_result_string**
+VP検証結果文字列
+Verifier による VP の検証結果文字列を指定する。
