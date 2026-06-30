85219e1c2ee36fa254eb4938c2a384c7766a021f
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-06-30 14:05:12 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,12 +1,19 @@
-**{walt.id Issuer側で定義された credential_configuration_id}**
-発行可能証明書ID
-walt.id Issuer 側で定義された credential_configuration_id ごとの証明書情報が指定される。
+**credentialOfferUrl**
+Credential Offer URL
+Issuer が発行した Credential Offer URL が指定される。
 
-**cryptographic_binding_methods_supported**
-サポートされているバインディング方式の一覧
-証明書に紐づけ可能な DID などのバインディング方式が指定される。
+証明書取得時に使用する Credential Offer URL が指定される。
+
+データが存在しない場合、null が指定される。
 
 
-**vct**
-証明書タイプ識別子
-証明書のフォーマット種別が vc+sd-jwt の場合、本項目が指定される。
+**credentialOfferEndpoint**
+Credential Offer Endpoint
+証明書取得時に使用する Credential Offer Endpoint の URL が指定される。
+
+データが存在しない場合、null が指定される。
+
+
+**targets**
+証明書を取得可能な対象者のユーザ名リスト
+証明書取得対象者として指定されたユーザ名の一覧が指定される。
