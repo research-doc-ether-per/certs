fc0079db47d619272b5ae99b0c16197aab097a90
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-06-30 15:44:26 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,11 +1,28 @@
-**概要**
 
-指定されたユーザについて、基本4情報証明書の発行リクエストを送信し、発行された基本4情報証明書を返却する。
+指定されたグループIDに対応する Verifier が発行した Presentation Request URL または提示可能な証明書の一覧を取得する。
 
-**image**
-基本4情報に設定する画像データ
-Base64 エンコードされた画像データを指定する。
 
-**jwt**
-発行された基本4情報証明書本体データ
-Disclosures 部分を含む JWT 形式のデータが指定される。
+
+指定された提示可能証明書IDに基づき、指定されたグループIDに対応する Verifier が発行した Presentation Request URL または提示可能な証明書情報を取得する。
+Presentation Request URL または提示可能な証明書が存在しない場合、空のオブジェクトが返却される。
+
+
+
+指定されたグループIDに対応する Verifier から、指定されたユーザに対して Presentation Request URL を発行する。
+発行された Presentation Request URL は保持され、対象ユーザに紐づけられる。
+
+
+
+指定されたグループIDに対応する Verifier から、指定された提示可能証明書IDに基づいて Presentation Request URL を発行する。
+
+
+指定されたグループIDに対応する Verifier について、対象ユーザを指定して提示可能証明書情報を登録する。
+発行された URL は保持され、対象ユーザに紐づけられる。
+
+
+指定されたグループIDに対応する Verifier について、VP を保存する。
+
+
+指定されたグループIDに対応する Verifier の保存済み VP 一覧を取得する。
+
+指定されたグループIDに対応する Verifier の保存済み VP を参照する。
