b829288f04feb207df21f65339c0ef4217959cb5
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-07-01 18:32:34 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,4 +1,1 @@
-
-調査したところ、Keycloak の IdP Broker 機能では、外部 IdP 側の group / realm role / client role は、ログイン先 Realm の token へ自動的には引き継がれないようです。
-
-そのため、現状では IdP Mapper を使用して固定的にマッピングする方法で対応する必要があります。
+一点確認ですが、組織ウォレットを利用する場合も、vc-issuer / vc-verifier 側で IdP の設定が必要になるという認識でよろしいでしょうか。
