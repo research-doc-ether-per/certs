d04689d9f2bc5425bd59ac1babfb7f94c8ed1d84
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-07-02 14:15:56 +0900

    Refactor user authentication with UserContext

--- verify.js
+++ verify.js
@@ -1,24 +1,88 @@
+'use client'
 
-export const getKeycloakConfigByPathname = (pathname) => {
-  if (pathname.startsWith('/individual')) {
-    return {
-      type: 'personal',
-      role: 'user',
-      url: process.env.NEXT_PUBLIC_KEYCLOAK_REALM_PERSONAL_URL,
-      realm: process.env.NEXT_PUBLIC_KEYCLOAK_REALM_PERSONAL_NAME,
-      clientId: process.env.NEXT_PUBLIC_KEYCLOAK_REALM_PERSONAL_CLIENT_ID,
+import { createContext, useEffect, useRef, useState } from 'react'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { keycloakInit } from '@/lib/keycloak'
+import { getKeycloakConfigByPathname } from '@/lib/keycloak-config'
+import logger from '@/lib/logger'
+
+export const UserContext = createContext(null)
+
+export function UserProvider({ children }) {
+  const router = useRouter()
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+
+  const [loading, setLoading] = useState(true)
+  const [realm, setRealm] = useState(null)
+  const [userInfo, setUserInfo] = useState(null)
+  const [userInfoRole, setUserInfoRole] = useState(null)
+
+  const isInitialized = useRef(false)
+
+  useEffect(() => {
+    initialize()
+  }, [pathname])
+
+  /**
+   * ユーザー認証を初期化
+   */
+  const initialize = async () => {
+    try {
+      if (isInitialized.current) return
+
+      if (typeof window === 'undefined') return
+
+      const keycloakConfig = getKeycloakConfigByPathname(pathname)
+
+      logger.debug('keycloakConfig:', keycloakConfig)
+
+      if (!keycloakConfig) {
+        setLoading(false)
+        return
+      }
+
+      const kc = await keycloakInit(keycloakConfig)
+
+      logger.debug('authenticated:', kc.authenticated)
+
+      if (!kc.authenticated) {
+        await kc.login({
+          redirectUri: window.location.href,
+        })
+        return
+      }
+
+      logger.debug('accessToken:', kc?.token)
+
+      const profile = await kc.loadUserInfo()
+
+      logger.debug('userInfo:', profile)
+      logger.debug('userId:', profile.sub)
+
+      setRealm(keycloakConfig.realm)
+      setUserInfo(profile)
+      setUserInfoRole(keycloakConfig.role)
+      setLoading(false)
+
+      isInitialized.current = true
+    } catch (error) {
+      logger.error('user-context.jsx >>> initialize error:', error)
+      setLoading(false)
+      isInitialized.current = true
     }
   }
 
-  if (pathname.startsWith('/vc_client')) {
-    return {
-      type: 'group',
-      role: 'manager',
-      url: process.env.NEXT_PUBLIC_KEYCLOAK_REALM_GROUP_URL,
-      realm: process.env.NEXT_PUBLIC_KEYCLOAK_REALM_GROUP_NAME,
-      clientId: process.env.NEXT_PUBLIC_KEYCLOAK_REALM_GROUP_CLIENT_ID,
-    }
-  }
-
-  return null
+  return (
+    <UserContext.Provider
+      value={{
+        loading,
+        realm,
+        userInfo,
+        userInfoRole,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  )
 }
