a5e6bbdab343383f20994fb901b9f568c885db3e
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-07-08 13:01:28 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,11 +1,1 @@
-
-components: {
-  MuiContainer: {
-    styleOverrides: {
-      root: ({ theme }) => ({
-        paddingLeft: `calc(clamp(${theme.spacing(2)}, 4vw, ${theme.spacing(4)}) + 16px)`,
-        paddingRight: `calc(clamp(${theme.spacing(2)}, 4vw, ${theme.spacing(4)}) + 16px)`,
-      }),
-    },
-  },
-},
+https://github.com/devias-io/material-kit-react/blob/v4.0.0/src/components/dashboard/overview/budget.tsx
