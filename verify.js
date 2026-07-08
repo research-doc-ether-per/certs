3d276228a40ed8f53ef83a0e0f57150e8597d989
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-07-08 12:48:50 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,1 +1,11 @@
 
+components: {
+  MuiContainer: {
+    styleOverrides: {
+      root: ({ theme }) => ({
+        paddingLeft: `calc(clamp(${theme.spacing(2)}, 4vw, ${theme.spacing(4)}) + 16px)`,
+        paddingRight: `calc(clamp(${theme.spacing(2)}, 4vw, ${theme.spacing(4)}) + 16px)`,
+      }),
+    },
+  },
+},
