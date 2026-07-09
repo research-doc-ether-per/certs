1b24d752fe5fce09d576463e2b6b37db82cc0aa6
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-07-09 13:48:44 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,13 +1,1 @@
-/**
- * Keycloak から取得した group path の配列を階層構造の object に変換する
- * 各階層は group 名を key として保持する。
- * 子 group が存在しない場合は空 object として保持する。
- *
- * @param {string[]} groups
- * Keycloak から取得した group path の配列。
- * 例: ['/group1/group1-3', '/group2/group2-1/group2-1-1']
- *
- * @returns {Object}
- * group path を階層構造に変換した object。
- * 各 group 名を key とし、子 group が存在しない場合は空 object を返す。
- */
+
