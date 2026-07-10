7cb8eb11e73a000f54201bb164736a5d5279578f
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-07-10 13:42:25 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,1 +1,15 @@
-
+/**
+ * 組織 Wallet 認証情報生成用のハッシュ値を生成する
+ *
+ * @param {string} value
+ * Access Token の iss と組織 Wallet ID を連結した文字列
+ *
+ * @returns {string}
+ * SHA-256 でハッシュ化した 16 進数文字列
+ */
+const createHash = (value) => {
+  return crypto
+    .createHash('sha256')
+    .update(value)
+    .digest('hex')
+}
