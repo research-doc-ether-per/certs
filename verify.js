2d00ea712a950bad26ea025d743b001af0853da9
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-07-13 19:08:24 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,6 +1,3 @@
-
-`usePresentationRequest` では、`did` が未指定の場合、デフォルト DID を使用して VP に署名するようです。
-ExchangeController.kt: https://github.com/walt-id/waltid-identity/blob/v0.15.1/waltid-services/waltid-wallet-api/src/main/kotlin/id/walt/webwallet/web/controllers/exchange/ExchangeController.kt#L111-L113
-
-また、`HolderBindingPolicy` の DID バインディング確認では、VC JWT Payload のルート階層にある `sub` フィールドを比較しています。
-そのため、JSON 内部の `credentialSubject.id` が存在しない場合でも、このポリシーの検証には影響しない認識です。
+Holder ウォレットは VC を VP としてまとめ、Holder 自身の秘密鍵でVP に署名します。
+そのため、VP 外側の iss は Holder の DID になります。
+VP 内の各 VC の sub については、後ほど debug で確認します。
