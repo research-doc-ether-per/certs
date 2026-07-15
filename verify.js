fcac4165b32b1be4c2316c06ee6644fd2e09aa24
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-07-15 18:21:59 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,11 +1,1 @@
-const authTypes = {
-  // 個人ユーザーまたはIssuer管理者専用API認証
-  individualOrIssuerAdmin: 'individualOrIssuerAdmin',
 
-  // 組織ユーザー専用API認証
-  organizationUser: 'organizationUser',
-
-    // ユーザー種別判定不要API認証
-  authenticatedUser: 'authenticatedUser',
-
-}
