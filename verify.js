717b3725f0df51d3c7a1216eb2e4a47a3e7d82cc
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-07-16 11:20:14 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,1 +1,34 @@
 
+{
+  "env": {
+    "node": true,
+    "es2021": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:node/recommended",
+    "plugin:prettier/recommended"
+  ],
+  "parserOptions": {
+    "ecmaVersion": 12,
+    "sourceType": "module"
+  },
+  "rules": {
+    "prettier/prettier": "error",
+    "no-console": "warn",
+    "no-unused-vars": [
+      "warn",
+      {
+        "argsIgnorePattern": "^_",
+        "varsIgnorePattern": "^_"
+      }
+    ],
+    "node/no-unsupported-features/es-syntax": [
+      "error",
+      {
+        "version": ">=12.0.0",
+        "ignores": []
+      }
+    ]
+  }
+}
