6f6615cdcec116896a114d6709a9267f682eaaa2
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-07-16 16:01:12 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,9 +1,42 @@
-const {
-  exists: statusListExists,
-  statusListUrl,
-} = await prepareStatusListUrl({
+/**
+ * Status List情報を登録する
+ *
+ * @param {Object} params パラメーター
+ * @param {Object} params.client PostgreSQLクライアント
+ * @param {Object} params.walletDBService Wallet DBサービス
+ * @param {string} params.groupId グループID
+ * @param {string} params.type 証明書タイプ
+ * @param {string} params.statusListUrl Status List URL
+ * @returns {Promise<void>}
+ */
+const registerStatusList = async ({
+  client,
   walletDBService,
   groupId,
   type,
-  createBSL,
-})
+  statusListUrl,
+}) => {
+  await walletDBService._insertWithConflictHandling(
+    client,
+    'status_list',
+    {
+      group_id: groupId,
+      type,
+      status_list_credential_url: statusListUrl,
+    },
+    [
+      'group_id',
+      'type',
+      'status_list_credential_url',
+    ]
+  )
+}
+
+
+ await registerStatusList({
+    client,
+    walletDBService,
+    groupId,
+    type,
+    statusListUrl,
+  })
