34620590023d661d489985f7c4d90836ef8617ce
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-07-16 13:35:42 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,19 +1,1 @@
- // 組織Credential Offer URL一覧取得
-  getOrgCredentialOfferUrlList:
-    authTypes.organizationUser,
 
-  // 組織Credential Offer URL情報取得
-  getOrgCredentialOfferUrlInfo:
-    authTypes.organizationUser,
-
-  // 組織Credential Offer URL発行
-  issueOrgCredentialOfferUrl:
-    authTypes.organizationUser,
-
-  // 証明書ID指定組織Credential Offer URL発行
-  issueOrgCredentialOfferUrlByCredentialId:
-    authTypes.organizationUser,
-
-  // 組織発行可能証明書情報登録
-  registerOrgIssuableCredential:
-    authTypes.organizationUser,
