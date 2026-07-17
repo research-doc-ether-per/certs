f4f6e476387d94c0f88415b913964e94fa7b0ecc
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-07-17 14:28:51 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,1 +1,9 @@
+const trimValue = (value) => {
+  return String(value ?? '').replace(/^[\s　]+|[\s　]+$/g, '')
+}
 
+
+  const apiName = trimValue(row[0])
+  const apiId = trimValue(row[1])
+  const apiPath = trimValue(row[2])
+  const method = trimValue(row[3])
