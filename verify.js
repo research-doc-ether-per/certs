8772b5d83f60b70a9f14b1160bc4131377dce3e2
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-07-21 16:18:07 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,1 +1,102 @@
 
+import {
+  TextField,
+  Typography,
+} from '@mui/material'
+
+const textFieldStyles = {
+  '& .MuiInputBase-input': {
+    whiteSpace: 'nowrap',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+  },
+}
+
+const textAreaStyles = {
+  '& .MuiInputBase-inputMultiline': {
+    whiteSpace: 'pre-wrap',
+    overflowWrap: 'anywhere',
+    wordBreak: 'break-word',
+  },
+}
+
+const requiredStyle = {
+  color: '#d63384',
+  paddingLeft: '3px',
+}
+
+/**
+ * 入力項目のラベル
+ *
+ * @param {Object} props プロパティ
+ * @returns {JSX.Element} ラベル
+ */
+export const TextFieldLabel = ({
+  title,
+  required = false,
+  ...others
+}) => (
+  <Typography
+    variant="subtitle1"
+    color="text.secondary"
+    gutterBottom
+    {...others}
+  >
+    {title}
+
+    {required ? (
+      <span style={requiredStyle}>*</span>
+    ) : null}
+  </Typography>
+)
+
+/**
+ * 1行入力用TextField
+ *
+ * @param {Object} props プロパティ
+ * @returns {JSX.Element} TextField
+ */
+export const CustomTextField = ({
+  value,
+  sx,
+  ...others
+}) => (
+  <TextField
+    value={value ?? ''}
+    fullWidth
+    variant="outlined"
+    {...others}
+    sx={{
+      ...textFieldStyles,
+      ...sx,
+    }}
+  />
+)
+
+/**
+ * 複数行入力用TextField
+ *
+ * @param {Object} props プロパティ
+ * @returns {JSX.Element} TextArea
+ */
+export const CustomTextArea = ({
+  value,
+  minRows = 3,
+  maxRows = 6,
+  sx,
+  ...others
+}) => (
+  <TextField
+    value={value ?? ''}
+    multiline
+    minRows={minRows}
+    maxRows={maxRows}
+    fullWidth
+    variant="outlined"
+    {...others}
+    sx={{
+      ...textAreaStyles,
+      ...sx,
+    }}
+  />
+)
