0db5f8ae22aad2f746b6da8c6681e3754f61f513
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-07-21 18:01:36 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,15 +1,1 @@
-const addIssueTarget = (
-  response,
-  issueTarget
-) => ({
-  ...response,
-  data: {
-    ...response?.data,
-    issueTarget,
-  },
-})
 
-response = addIssueTarget(
-  response,
-  issueTargets.organization
-)
