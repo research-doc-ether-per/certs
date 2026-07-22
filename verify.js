8f13f50a66a9d810ed01eed4b683ab60ae510884
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-07-22 12:47:54 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,11 +1,1 @@
-/**
- * VC Registry APIのアクセストークンを取得する
- *
- * @returns {Promise<string>} アクセストークン
- */
-const getVcRegistryApiToken = async () => {
-  return getClientCredentialsToken({
-    realmName: 'groupcertAuth',
-    clientID: 'vc-registry-api',
-  })
-}
+
