1725b95b2b7c8340b0077b5f12701f9103c4fb7b
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-07-22 18:04:28 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,8 +1,1 @@
-if (baseInfo) {
-  baseInfo.credentialOfferEndpointName =
-    await getCredentialOfferEndpointName({
-      walletDBPool: walletDBService.walletDBPool,
-      groupId,
-      credentialOfferEndpoint: baseInfo.credentialOfferEndpoint,
-    })
-}
+
