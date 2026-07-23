99032d4eb22ff8bf88e4cae94eeb1cc3134aa3aa
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-07-23 13:10:36 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,1 +1,16 @@
 
+# ルーターリスト
+titles:
+  header: Cloud Credential Service 個別Verifierサイト
+
+  presentationDetail: 受領済み証明書詳細
+  presentationList: 受領済み証明書一覧
+
+  presentationRequestUrlList: Presentation Request URL一覧
+  presentationRequestUrlDetail: Presentation Request URL詳細
+
+  issuePresentationRequestUrl: Presentation Request URL発行依頼
+  issuePersonalPresentationRequestUrl: 個人Presentation Request URL発行依頼
+  issueOrgPresentationRequestUrl: 組織Presentation Request URL発行依頼
+
+  issuedPresentationRequestUrlConfirm: Presentation Request URL確認
