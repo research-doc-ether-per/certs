248a90d9ec416816fa47d6402e4e8f9fd594727f
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-07-24 13:26:07 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,1 +1,65 @@
 
+/**
+ * Presentation Request URL 一覧レスポンスを生成する
+ *
+ * @param {Object[]} datas
+ * DB 取得データ
+ *
+ * @param {string} targetKey
+ * targets に設定する対象項目の key
+ * 例: userId / orgWalletId
+ *
+ * @returns {Object}
+ * Presentation Request URL 一覧レスポンス
+ */
+const createPresentationRequestUrlListResponse = (datas = [], targetKey) => {
+  const tempObj = new Map()
+
+  for (const data of datas) {
+    const {
+      id,
+      groupId,
+      presentationRequestUrl,
+      state,
+      type,
+      format,
+      name,
+      issuedByUserId,
+      updateDate,
+      createDate,
+    } = data
+
+    const targetValue = data[targetKey]
+
+    if (!tempObj.has(id)) {
+      tempObj.set(id, {
+        id,
+        groupId,
+        presentationRequestUrl,
+        state,
+        type,
+        format,
+        name,
+        issuedByUserId,
+        targets: [],
+        updateDate,
+        createDate,
+        _targetSet: new Set(),
+      })
+    }
+
+    const obj = tempObj.get(id)
+
+    if (targetValue && !obj._targetSet.has(targetValue)) {
+      obj._targetSet.add(targetValue)
+      obj.targets.push(targetValue)
+    }
+  }
+
+  const list = Array.from(tempObj.values()).map((item) => {
+    delete item._targetSet
+    return item
+  })
+
+  return { list }
+}
