ccf75202625538904d2dfaf216fbcb19ba414d2d
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-07-27 10:51:34 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,1 +1,24 @@
+/**
+ * Realm 名に応じてリクエスト用ユーザIDを取得する
+ *
+ * @param {Object} params パラメータ
+ * @param {string} params.realmName Keycloak Realm 名
+ * @param {Object} params.kcUser Keycloak ユーザ情報
+ * @param {string} params.userId ユーザIDまたはグループID
+ * @returns {string} リクエスト用ユーザID
+ */
+const getRequestUserIdByRealmName = ({ realmName, kcUser, userId }) => {
+  if (realmName === keycloak.allowedRealms.individual) {
+    return userId
+  }
 
+  if (realmName === keycloak.allowedRealms.organization) {
+    return `${kcUser.iss}${userId}`
+  }
+
+  const error = new Error(`Unsupported realm. realmName: ${realmName}`)
+  error.code = 'InvalidParamsError'
+  error.params = [realmName]
+
+  throw error
+}
