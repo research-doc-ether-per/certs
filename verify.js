23db68218305f65ea7326600da55f6e420d2459f
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-07-27 18:25:37 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,1 +1,40 @@
 
+/**
+ * Presentation 対象の Credential 情報を生成する
+ *
+ * @param {Object[]} matchedCredentials Presentation Definition に一致した Credential 一覧
+ * @returns {Promise<Object>} Presentation 対象 Credential 情報
+ */
+const createMatchedCredentialInfo = async (matchedCredentials = []) => {
+  const selectedCredentials = []
+  const rsIds = []
+  const disclosures = {}
+
+  for (const credential of matchedCredentials) {
+    selectedCredentials.push(credential.id)
+
+    if (credential.disclosures) {
+      disclosures[credential.id] = Array.isArray(credential.disclosures)
+        ? credential.disclosures
+        : [credential.disclosures]
+    }
+
+    const datas = await walletDBService.select('vc_wallets', {
+      waltid_credential_id: credential.id,
+    })
+
+    rsIds.push(datas[0]?.rsId)
+  }
+
+  return {
+    selectedCredentials,
+    disclosures,
+    rsIds,
+  }
+}
+
+const {
+  selectedCredentials,
+  disclosures,
+  rsIds,
+} = await createMatchedCredentialInfo(matchedCredentials)
