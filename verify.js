7126921138882c821a89385849ef2c88530d7a10
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-07-27 15:12:23 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,1 +1,11 @@
 
+const {
+  certName,
+  docId,
+  imageUrl,
+  name,
+  did: issuerDid,
+  groupId,
+} = credentialIssuerInfo
+
+let credential_offer_url = credentialIssuerInfo.credentialOfferUrl
