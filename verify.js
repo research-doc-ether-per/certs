01aa86bda8033d05296ba603b1b19d462ab6e359
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-07-28 11:41:19 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,15 +1,43 @@
-const verifyResult = {
-  presentation,
-  credentials,
-}
+/**
+ * Presentation Request を解決し、Presentation Request 情報を取得する
+ *
+ * @param {Object} params パラメータ
+ * @param {string} params.walletId Wallet ID
+ * @param {string} params.accessToken Walt.id Wallet API アクセストークン
+ * @param {string} params.presentationUri Presentation Request URL
+ * @returns {Promise<Object>} Presentation Request 情報
+ */
+const resolvePresentationRequestInfo = async ({
+  walletId,
+  accessToken,
+  presentationUri,
+}) => {
+  let presentationRequest
 
-await saveVpResult({
-  realmName,
-  groupId,
-  requestBody: {
-    state,
-    vpToken,
-    userName,
-    verifyResult,
-  },
-})
+  try {
+    presentationRequest = await resolvePresentationRequest({
+      walletId,
+      accessToken,
+      presentationUri,
+    })
+  } catch (error) {
+    if (
+      String(error.status) === '500' &&
+      error.response?.data?.id === 'AuthorizationError'
+    ) {
+      error.code = 'PresentationRequestUrlExpiredError'
+    }
+
+    throw error
+  }
+
+  const presentationUrl = new URL(presentationRequest)
+  const presentationDefinition = presentationUrl.searchParams.get(
+    'presentation_definition'
+  )
+
+  return {
+    presentationRequest,
+    presentationDefinition,
+  }
+}
