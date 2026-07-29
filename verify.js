76816d1c6078dbdd3c35b74fa02798d022427e0b
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-07-29 16:46:29 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,15 +1,1 @@
-/**
- * 基本4情報 Credential の定義と一致するか判定する
- *
- * @param {Object} params パラメータ
- * @param {string | null} params.vctLastPath VCT の最後のパス
- * @param {string[]} params.type Credential Definition Type
- * @returns {boolean} 基本4情報 Credential の定義と一致する場合 true
- */
-const matchesBase4InfoCredential = ({ vctLastPath, type }) => {
-  return (
-    vctLastPath === BASE_4_INFO_VCT &&
-    JSON.stringify(type) ===
-      JSON.stringify(BASE_4_INFO_CREDENTIAL_DEFINITION_TYPE)
-  )
-}
+
