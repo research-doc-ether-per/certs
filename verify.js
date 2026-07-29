648adb237b751c3188e5d640689fade739e7d713
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-07-29 18:12:52 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,1 +1,13 @@
-const ALL_SETTING = 'all'
+/**
+ * 認可方式の表示用 object を生成する
+ *
+ * @param {string[]} authTypes 認可方式一覧
+ * @param {Object} authTypeLabels 認可方式表示名定義
+ * @returns {Object} 認可方式表示用 object
+ */
+const createAuthTypeDisplayObject = (authTypes = [], authTypeLabels = {}) => {
+  return authTypes.reduce((result, authType) => {
+    result[authType] = authTypeLabels[authType] || authType
+    return result
+  }, {})
+}
