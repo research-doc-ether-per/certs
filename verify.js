004a57cce49247ec1e0dddcc36cd18f141ebbe5e
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-07-31 16:43:27 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,1 +1,55 @@
 
+/**
+ * Presentation Request の input_descriptor を生成する
+ *
+ * @param {Object} params パラメータ
+ * @param {string} params.id input_descriptor の識別子
+ * @param {string} params.format VC フォーマット
+ * @param {string} params.path 検証対象の JSONPath
+ * @param {Object} params.filter filter 定義
+ * @returns {Object} input_descriptor
+ */
+const createInputDescriptor = ({ id, format, path, filter }) => {
+  return {
+    input_descriptor: {
+      id,
+      format: {
+        [format]: {},
+      },
+      constraints: {
+        fields: [
+          {
+            path: [path],
+            filter,
+          },
+        ],
+      },
+    },
+  }
+}
+
+
+
+createInputDescriptor({
+  id: type,
+  format: 'jwt_vc_json',
+  path: '$.vc.type',
+  filter: {
+    type: 'array',
+    contains: {
+      type: 'string',
+      const: type,
+    },
+  },
+})
+
+
+createInputDescriptor({
+  id: vctValue,
+  format: 'vc+sd-jwt',
+  path: '$.vct',
+  filter: {
+    type: 'string',
+    const: vctValue,
+  },
+})
