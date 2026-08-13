e316fd91e557c75f6fc7a5b27227acaa61c8680f
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-08-13 17:01:14 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,3 +1,1 @@
-Keycloak の Web 用 Client 設定を確認し、ユーザーのロールに応じて Valid Redirect URIs、Root URL、Home URL を設定しました。
-また、一部 Client で不足していた Valid Post Logout Redirect URIs も追加しました。
-上記対応後、ログアウト時の invalid redirect URL は発生しなくなりました。
+
