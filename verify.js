c1e7400d5a843e74e79b5a511fe9a32e7d12c4db
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-08-14 11:00:55 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,19 +1,1 @@
-package id.walt.issuer.config
 
-import id.walt.commons.config.WaltConfig
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class OAuthConfig(
-    val name: String,
-    val authorizeUrl: String,
-    val accessTokenUrl: String,
-    val clientId: String,
-    val clientSecret: String
-)
-
-@Serializable
-data class AuthenticationServiceConfig(
-    val individual: OAuthConfig,
-    val corporate: OAuthConfig
-) : WaltConfig()
