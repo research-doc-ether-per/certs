9341807ebcbb6ef84c51780f2af2d3e454b92396
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-08-14 11:18:34 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,52 +1,37 @@
-package id.walt.issuer.web.plugins
-
-import id.walt.commons.config.ConfigManager
-import id.walt.commons.web.modules.AuthenticationServiceModule
-import id.walt.issuer.config.AuthenticationServiceConfig
-import id.walt.issuer.config.OIDCIssuerServiceConfig
-import io.ktor.client.*
-import io.ktor.http.*
-import io.ktor.server.auth.*
-
-val issuerAuthenticationPluginAmendment: suspend () -> Unit = suspend {
-    val authenticationServiceConfig = ConfigManager.getConfig<AuthenticationServiceConfig>()
-    val issuerServiceConfig = ConfigManager.getConfig<OIDCIssuerServiceConfig>()
-
-    AuthenticationServiceModule.AuthenticationServiceConfig.apply {
-        customAuthentication = {
-            // 1. 個人用 OAuth Providerer
-            oauth("auth-oauth-individual") {
-                client = HttpClient()
-                providerLookup = {
-                    OAuthServerSettings.OAuth2ServerSettings(
-                        name = authenticationServiceConfig.individual.name,
-                        authorizeUrl = authenticationServiceConfig.individual.authorizeUrl,
-                        accessTokenUrl = authenticationServiceConfig.individual.accessTokenUrl,
-                        clientId = authenticationServiceConfig.individual.clientId,
-                        clientSecret = authenticationServiceConfig.individual.clientSecret,
-                        requestMethod = HttpMethod.Post
-                    )
-                }
-
-                urlProvider = { "${issuerServiceConfig.baseUrl}/callback" }
-            }
-
-            // 2. 組織用 OAuth Provider
-            oauth("auth-oauth-corporate") {
-                client = HttpClient()
-                providerLookup = {
-                    OAuthServerSettings.OAuth2ServerSettings(
-                        name = authenticationServiceConfig.corporate.name,
-                        authorizeUrl = authenticationServiceConfig.corporate.authorizeUrl,
-                        accessTokenUrl = authenticationServiceConfig.corporate.accessTokenUrl,
-                        clientId = authenticationServiceConfig.corporate.clientId,
-                        clientSecret = authenticationServiceConfig.corporate.clientSecret,
-                        requestMethod = HttpMethod.Post
-                    )
-                }
-
-                urlProvider = { "${issuerServiceConfig.baseUrl}/callback" }
-            }
+if (FeatureManager.isFeatureEnabled(FeatureCatalog.legacyAuthenticationFeature)) {
+    // 1. 注册个人用 Keycloak
+    oauth("auth-oauth-individual") {
+        client = HttpClient()
+        providerLookup = {
+            OAuthServerSettings.OAuth2ServerSettings(
+                name = authenticationServiceConfig.individual.name,
+                authorizeUrl = authenticationServiceConfig.individual.authorizeUrl,
+                accessTokenUrl = authenticationServiceConfig.individual.accessTokenUrl,
+                clientId = authenticationServiceConfig.individual.clientId,
+                clientSecret = authenticationServiceConfig.individual.clientSecret,
+                accessTokenRequiresBasicAuth = false,
+                requestMethod = HttpMethod.Post,
+                defaultScopes = listOf("openid", "profile", "email")
+            )
         }
+        urlProvider = { "${oidcConfig.publicBaseUrl}/wallet-api/auth/oidc-session" }
+    }
+
+    // 2. 注册组织用 Keycloak
+    oauth("auth-oauth-corporate") {
+        client = HttpClient()
+        providerLookup = {
+            OAuthServerSettings.OAuth2ServerSettings(
+                name = authenticationServiceConfig.corporate.name,
+                authorizeUrl = authenticationServiceConfig.corporate.authorizeUrl,
+                accessTokenUrl = authenticationServiceConfig.corporate.accessTokenUrl,
+                clientId = authenticationServiceConfig.corporate.clientId,
+                clientSecret = authenticationServiceConfig.corporate.clientSecret,
+                accessTokenRequiresBasicAuth = false,
+                requestMethod = HttpMethod.Post,
+                defaultScopes = listOf("openid", "profile", "email")
+            )
+        }
+        urlProvider = { "${oidcConfig.publicBaseUrl}/wallet-api/auth/oidc-session" }
     }
 }
