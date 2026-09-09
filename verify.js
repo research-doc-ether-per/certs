85349b4b6b54b2bea9b29350be4e7638d6e76358
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-09-09 14:42:18 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,10 +1,1 @@
-docker cp docker-compose-wallet-api2-1:/waltid-wallet-api2/wallet2.db ./wallet2.db
-
-sudo apt update
-sudo apt install sqlite3
-
-
-sqlite3 wallet2.db ".tables"
-
-
-sqlite3 wallet2.db .dump > wallet2_dump.sql
+sqlite3 wallet2.db ".schema" > wallet2_schema.sql
