0d817ffa5d809382a18a524d86f81fc887fc39c1
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-09-09 19:03:25 +0900

    Add investigation results and future tasks
    整理した差分調査結果と今後の調査内容を追加しました。

--- verify.js
+++ verify.js
@@ -1,1 +1,8 @@
 
+本日は walt.id v0.15.1 と v1.0.0 の差分調査結果 を整理しました。
+
+明日からは Sample を作成しながら、以下の内容を順番に調査します。
+1. Wallet2 のユーザー登録・ログイン・認証機能
+2. Issuer2 における Issuer 情報の作成方法
+3. Issuer2 の Credential Offer 作成および Credential 発行
+4. Issuer2 における ID Token の利用方法
