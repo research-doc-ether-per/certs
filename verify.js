972364534b9bd4d7f1998d5bf34fe120635ed938
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-09-14 13:22:22 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,1 +1,9 @@
 
+Wallet2 v1.0.0 では、以下の Wallet 管理 API が提供されています。
+
+| Method | API | 説明 |
+|---|---|---|
+| `POST` | `/wallet` | 新しい Wallet を作成する。 |
+| `GET` | `/wallet` | Wallet の一覧を取得する。 |
+| `GET` | `/wallet/{walletId}` | 指定した Wallet の詳細情報を取得する。 |
+| `DELETE` | `/wallet/{walletId}` | 指定した Wallet を削除する。 |
