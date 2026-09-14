0aee54c2356c0e8205632af1c22006fce54feeed
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-09-14 18:39:30 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,1 +1,22 @@
 
+タイトル
+各 Web 画面のタイトルが旧名称のまま表示されている
+
+解析内容
+現状、各 Web 画面のタイトルに旧名称である Cloud Credential Service が表示されている。
+
+仕様上、画面タイトルは SmartLiTA Wallet に変更する必要があるが、一部の Web 画面では旧名称のままとなっている。
+
+そのため、対象となる Web 画面について、タイトル表示を Cloud Credential Service から SmartLiTA Wallet に変更する必要がある。
+
+水平展開結果
+wallet-web：タイトルを Cloud Credential Service から SmartLiTA Wallet に変更する必要がある。
+
+issuer-web：タイトルを Cloud Credential Service から SmartLiTA Wallet に変更する必要がある。
+
+verifier-web：タイトルを Cloud Credential Service から SmartLiTA Wallet に変更する必要がある。
+
+対応内容
+各 Web 画面のタイトル表示を修正する。
+
+Cloud Credential Service と表示されている箇所を、SmartLiTA Wallet に変更する。
