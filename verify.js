d43246505f294056e1a456335a1107e094d652ca
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-09-17 11:46:52 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,1 +1,31 @@
 
+/**
+ * Credential の詳細を取得する。
+ */
+const getCredential = async (
+  walletId,
+  credentialId,
+  accessToken = null
+) => {
+  logger.debug('*** getCredential start ***')
+
+  try {
+    const response = await fetchService.handleGet(
+      fetchService.walletApi2,
+      `/wallet/${walletId}/credentials/${credentialId}`,
+      accessToken
+    )
+
+    const result = response.data || {}
+
+    logger.debug('result : ', result)
+
+    return result
+  } catch (error) {
+    logger.error('error.message: ', error.message)
+    logger.error('error.stack: ', error.stack)
+    throw error
+  } finally {
+    logger.debug('*** getCredential end ***')
+  }
+}
