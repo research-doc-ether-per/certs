59011d9af519c244f3532b6f65a62cb208cbe531
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-09-17 18:19:06 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,8 +1,1 @@
-### 4.2.4 Authorization Code Flow の Callback URL について
 
-v0.15.1 と v1.0.0 では、Callback 処理および Callback URL が以下のように変更されています。
-
-| バージョン | Callback URL | 対応 |
-| --- | --- | --- |
-| v0.15.1 | `http://10.0.2.15:7002/callback` | 正常に Callback 処理を行うため、walt.id のソースコードを一部修正する必要があります。 |
-| v1.0.0 | `http://10.0.2.15:7005/openid4vci/external/oauth/callback` | walt.id のソースコードを修正する必要はありません。 |
