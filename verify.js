014313c72786269c021a00e39e2814651f8f42b8
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-09-24 13:34:35 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,1 +1,11 @@
 
+waltid-cli v1.0.0を確認したところ、DID作成は `did:key` と `did:jwk` のみ対応しており、`did:web` は対応していないようです。
+
+参照：
+- DidCreateCmd.kt
+  https://github.com/walt-id/waltid-identity/blob/v1.0.0/waltid-applications/waltid-cli/src/commonMain/kotlin/id/walt/cli/commands/DidCreateCmd.kt
+  `The native crypto2 DID method to use: key or jwk.`
+
+- waltid-cli README
+  https://github.com/walt-id/waltid-identity/blob/v1.0.0/waltid-applications/waltid-cli/README.md
+  `CLI creation advertises only the native crypto2 did:key and did:jwk registrars; former non-native method options were removed.`
