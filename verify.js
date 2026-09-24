f1d73cdd2bc30772c4d02a1b2f9fe8cd7143fbf8
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-09-24 10:50:25 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,1 +1,11 @@
+    "userId" = "THIS WILL BE REPLACED WITH ID TOKEN CLAIM"
+    "userName" = "THIS WILL BE REPLACED WITH ID TOKEN CLAIM"
 
+idTokenClaimsMapping = {
+  "$.userId" = "$.credentialSubject.userId"
+  "$.userName" = "$.credentialSubject.userName"
+}
+
+
+
+  idTokenClaimsMapping: config.idTokenClaimsMapping ?? null,
