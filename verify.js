3e53fd4d9137beff6e11802a1c37ecd0d655eaf0
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-09-25 18:59:19 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,8 +1,1 @@
 
-val credentialType = vct.substringAfterLast("/")
-
-val dynamicCredentialStatus = CredentialStatusProviderRegistry.provide(
-    credential = mappedCredentialData,
-    issuerId = issuerId,
-    credentialType = credentialType,
-)
