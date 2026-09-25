554a044887edaf786f4c4feced243a48bc047931
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-09-25 21:48:38 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,13 +1,1 @@
-{
-  policy: 'credential-status',
-  args: {
-    discriminator: 'w3c-list',
-    list: [
-      {
-        value: 0,
-        purpose: 'revocation',
-        type: 'BitstringStatusListEntry',
-      },
-    ],
-  },
-}
+
