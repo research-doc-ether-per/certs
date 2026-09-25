4579ce6045610c927ac4178704a5a549214dfe67
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-09-25 21:11:47 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,1 +1,11 @@
 
+const beforeIds = new Set(
+  credentialsBefore.map(credential => credential.id)
+)
+
+const addedCredentialIds = credentialsAfter
+  .filter(credential => !beforeIds.has(credential.id))
+  .map(credential => credential.id)
+
+logger.debug('今回追加された Credential ID:')
+logger.debug(JSON.stringify(addedCredentialIds, null, 2))
