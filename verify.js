d127df76c25ec76298b38c974bd758d0b37bbd20
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-09-25 14:46:12 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,1 +1,8 @@
 
+println("========== createCredentialResponse ==========")
+println("configuration.format = ${configuration.format}")
+println("issuerId = $issuerId")
+println("credentialData = $credentialData")
+println("dataMapping = $dataMapping")
+println("handler = ${handler::class}")
+println("==============================================")
