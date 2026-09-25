b4fb90db8f47200d2306e37f1a543a12075d0877
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-09-25 15:50:19 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,9 +1,1 @@
-println("========== mapped ==========")
-println("mapped = $mapped")
-println("============================")
 
-
-println("========== SD-JWT after mapping ==========")
-println("sdPayload.fullPayload = ${sdPayload.fullPayload}")
-println("sdPayload.undisclosedPayload = ${sdPayload.undisclosedPayload}")
-println("==========================================")
