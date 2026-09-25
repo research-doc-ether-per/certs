0966c0d56e48cea446278a649e28d033e62f73ad
AWA-DEV <44774769+awadev-cloud@users.noreply.github.com>
2026-09-25 13:53:22 +0900

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,1 +1,10 @@
 
+private val issuerService = BaseFeature(
+    "issuer-service",
+    "Issuer Service Implementation",
+    mapOf(
+        "issuer-service" to Issuer2ServiceConfig::class,
+        "vc-status" to VcStatusConfig::class,
+    )
+)
+
